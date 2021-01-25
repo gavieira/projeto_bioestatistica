0482cfaac843aba63f7f442c51594a5de973b2ea
gavieira <gabrieldeusdeth@gmail.com>
2021-01-25 19:56:37 -0300

    Aula 2 corrigida e exercícios parcialmente finalizados

--- tutorial_r/aula2.R
+++ tutorial_r/aula2.R
@@ -56,7 +56,7 @@ DF2 = DF %>%
   mutate(uptake_mm = uptake / 1000) %>% #Obs: mutate escreve por cima se já tiver uma coluna com aquele nome
   select(Plant, Type, Treatment, uptake_mm) %>% # O select seleciona as colunas na ordem que vc quiser.
   select(-Type, -Plant) %>% # Removendo colunas que vc não quer...
-  arange(-uptake_mm) %>% #ordena pela coluna. O - ordena do maior valor para o menor
+  arrange(-uptake_mm) %>% #ordena pela coluna. O - ordena do maior valor para o menor
   slice(1:5) # Pegando apenas os 5 maiores valores
 
 DF_Quebec_Chilled = DF %>%
@@ -73,7 +73,7 @@ DF_Final = rbind(DF_Quebec_Chilled, DF_Quebec_NonChilled)
 # Podemos usar o group_by
 
 DF_Medias = DF %>%
-  group_by(Type, Treatment) %>% #agupando por 2 colunas
+  group_by(Type, Treatment) %>% #agrupando por 2 colunas
  summarise(media_uptake = mean(uptake), #e sumarisando eles
            sd_uptake = sd(uptake)) %>%
   rename(Uptake = media_uptake) #Renomeando uma coluna
@@ -88,18 +88,16 @@ DF_Medias = DF %>%
 
 #Para abrir o arquivo, vc pode dar o full_path ou relative_path
 
-arquivo_tabela = "/home/gabriel/Dropbox/repos/projeto_bioestatistica/"
+arquivo_tabela = "./tutorial_r/dados/celulas.csv"
 
-dados = read_delim(arquivo_tabela, delim = "\t") # O arquvico é tsv, não csv
+dados = read_delim(arquivo_tabela, delim = "\t") # O arquivo é tsv, não csv
 
 #se eu fiz várias modificações e quero salvar o arquivo modificado, uso a função "write"
 
-write_delim(dados, "/home..../células2.csv", delim = ";") #Salvando com ';' como delimitador
+write_delim(dados, "tutorial_r/dados/células2.csv", delim = ";") #Salvando com ';' como delimitador
 
 #Mudando o working directory do R:
-
-setwd("/home/.../dir")
-
+setwd("tutorial_r/dados/")
 #Agora, vc pode abrir com caminhos relativos
 
 
@@ -109,7 +107,7 @@ install.packages("readxl")
 
 library(readxl)
 
-outra_tabela = read_excel("../../comportamento.xlsx")
+outra_tabela = read_excel("comportamento.xlsx")
 
 
 #Merging tables
@@ -128,17 +126,17 @@ dados_completos = merge(dados_celulas, dados_comportamento,
 
 #Rodando um teste t
 
-DF = dads_celulas %>%
-  filter(Structure %in% c("Cerebellum", "Hippocampus")) %>%
+DF = dados_celulas %>%
+  filter(Structure %in% c("Cerebellum", "Hippocampus")) %>% # somente linhas onde a coluna 'Structure' tem como valores "Cerebellum" e "Hippocampus" 
   group_by(Structure) %>%
   summarise(media_celulas = mean(Cells, na.rm = T))
   
 #OBS: Se tem NA ali, o R não calcula a média. Mas vc pode lidar ativamente com os NAs usando parametros. na.rm = T leva à remoção dos NAs da tabela.
 #Outra opção seria filtrar dados que não apresentam NA.
 
-DF = dads_celulas %>%
+DF = dados_celulas %>%
   filter(Structure %in% c("Cerebellum", "Hippocampus"), 
-         !is.na(Cells)) %>% # ! é o NOT do R
+         !is.na(Cells)) %>% # ! é o NOT do R. No caso, remove linhas que têm NA na coluna 'Cells'
   group_by(Structure) %>%
   summarise(media_celulas = mean(Cells, na.rm = T))
 
@@ -164,11 +162,112 @@ resultado$conf.level
 
 ## ALTERNATIVAMENTE, pegar o atributo te retorna o vetor direto, então dá pra fazer isso em menos linhas de códigos.
 
-cells_cb$Cells # É A MESMA COISA DE USAR O PULL
+dados_celulas$Cells # É A MESMA COISA DE USAR O PULL
 
-resultado2 = t.teste(cells_cb$Cells, cells_hp$Cells)
+resultado2 = t.test(dados_celulas$Cells, dados_celulas$Cells)
 
 #datatable - pacote de datascience mto performático, mas a sintaxe do tidyverse parece ser mais simples
 #Sempre lembrar do bioconductor
 #Bibliometrix - bibliometria
 #Outros pacotes do Hadley - Ex: lidar com datas
+
+
+
+#######################################
+
+
+#EXERCÍCIOS
+
+#Os exercícios abaixo usam tabelas disponíveis na pasta dados (A-E). Todas as tabelas contém os mesmos dados, mas cada uma em um formato diferente. O desafio é conseguir fazer com que o R leia elas no formato correto. Caso uma dessas tabelas seja carregada com sucesso, ela deve possuir 6 observações, em 2 colunas. O código abaixo serve de teste (todas as linhas devem imprimir TRUE se estiver tudo certo):
+
+library('tidyverse')
+library('readxl')
+
+getwd()
+setwd("tutorial_r/dados")
+  
+dados = read_delim("tabelaA.csv", delim = ',') # SEU CÓDIGO PRA LER A TABELA
+dados = read_delim("tabelaB.csv", delim = ';')
+dados = read_delim("tabelaC.tsv", delim = '\t')
+dados = read_excel("tabelaD.xlsx")
+dados = read_excel("tabelaE.xlsx", sheet = 2)
+
+# número de linhas e colunas: 6 e 2
+nrow(dados) == 6; ncol(dados) == 2
+
+# coluna X é numérica, coluna Y é texto/string/caracteres
+class(dados$X) == "numeric"; class(dados$Y) == "character"
+
+# média da coluna *X* é 30
+mean(dados$X, na.rm = T) == 30
+
+# quantidade de NAs na coluna *Y* é 1
+sum(is.na(dados$Y)) == 1
+
+# 1) Arquivo: tabelaA.csv
+# 2) Arquivo: tabelaB.csv
+# 3) Arquivo: tabelaC.tsv
+# 4) Arquivo: tabelaD.xlsx
+# 5) Arquivo: tabelaE.xlsx
+
+# Notem que no Windows, os caminhos são especificados com barra invertida (e.g. “Users\Kleber\Documentos”). O R usa barras normais (/), então cuidado na hora de copiar caminhos.
+
+
+######################################
+
+
+#Os exercícios abaixo usam os verbos comuns de dplyr pra manipulação de dados.
+
+#DATASET: mtcars (R base)
+
+library(tidyverse)
+View(mtcars)
+
+# • Imprima na tela a coluna hp. Dica: use a função select().
+
+mtcars %>% select(hp)
+
+# • Imprima na tela todas as colunas, exceto a coluna hp.
+
+mtcars %>% select(-hp)
+
+# • Crie um novo data frame mtcars2 que contém só as colunas hp, drat e wt. Dica: a função select() aceita vetores numéricos (e.g. select(5:6) seleciona da 5ª à 6ª coluna)
+
+mtcars2 <- mtcars %>% select(c(hp, drat, wt))
+mtcars2 <- mtcars %>% select(4:6)
+
+# • Crie as seguintes colunas em mtcars: kpg (kilômetros por galão - use 1 milha = 1.6 km), kpl (kilômetros por litro - use 1 galão = 3.8 litros). Dica: use a função mutate(). Tente fazer escrevendo mutate() uma única vez.
+
+mtcars %>%
+  mutate(kpg = mpg * 1.6, kpl = mpg * 1.6 / 3.8 )
+
+# • Crie um novo data frame mtcars3 que só tem os carros que tem mpg > 20. Dica: use a função filter().
+
+
+# • Crie um novo data frame mtcars4 que só tem os carros que tem mpg > 20 ou hp > 150. Dica: em R, a barra vertical (|) significa "ou" (o E comercial (&) significa "e", mas não precisa dele aqui).
+
+
+
+
+
+
+
+
+#DATASET: msleep.csv (disponível na pasta)
+
+# • Carregue os dados da tabela "msleep.csv" num data frame chamado msleep.
+
+
+# • Crie um data frame msleep_primates contendo apenas os primatas.
+
+
+# • Faça o data frame conter apenas a coluna sleep_total dos primatas. Escreva isso como continuação da linha de cima.
+
+
+# • Escreva uma linha que imprima na tela o número de primatas. Dica: use a função nrow().
+
+
+# • Crie uma variável sono_medio que contenha a duração média do sono de primatas.
+
+
+# • Calcule a média de sleep_total e a média de brainwt para todas as outras ordens (coluna order), excluindo primatas, e junte tudo em um data frame. Dica: use as funções group_by() e summarise().
